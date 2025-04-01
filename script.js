2baa2dbd268c167a61c473d8ed82710ca9ff2d16
eduardoreyes007351208 <lalosoccer307@gmail.com>
2025-04-01 11:50:23 -0700

    feat: Create and style the calc buttons

--- script.js
+++ script.js
@@ -12,4 +12,17 @@ let btnContHeight = parseFloat(btnContStyle.height
                     .filter((char) => numFilter.includes(char))
                     .join(''))
 
+
+let createBtns = () => {
+    let i = 0
+    while (i < 20) {
+        let btn = document.createElement('div')
+        btn.classList.add(`btn${i}`)
+        btn.setAttribute('style', `background: grey; width: ${btnContWidth/6}px; height: ${btnContHeight/7}px; border-radius: 5px;`)
+        btnContainer.appendChild(btn)
+        i++
+    }
+}
+
+createBtns()
 console.log(btnContWidth, btnContHeight)
