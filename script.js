e532cb7dae1fe7dd3a786a31d78d0a7975fdb463
eduardoreyes007351208 <lalosoccer307@gmail.com>
2025-04-01 11:25:48 -0700

    feat: Get width and height for buttons container
    Using the Styles, get the width and height for the
    buttons container and parse into float to be used
    in future calculations.

---
+++ script.js
@@ -0,0 +1,15 @@
+const btnContainer = document.querySelector('#btnContainer')
+const btnContStyle = getComputedStyle(btnContainer)
+const numFilter = '1234567890.'
+
+/* get the width and height from Styles for button container */
+let btnContWidth = parseFloat(btnContStyle.width
+                    .split('')
+                    .filter((char) => numFilter.includes(char))
+                    .join(''))
+let btnContHeight = parseFloat(btnContStyle.height
+                    .split('')
+                    .filter((char) => numFilter.includes(char))
+                    .join(''))
+
+console.log(btnContWidth, btnContHeight)
